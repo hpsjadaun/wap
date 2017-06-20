580344a174b7f88e8259d72a245208faacc665d7
hpsjadaun <hpjadaun@yahoo.com>
2017-06-20 16:46:10 +0530

    pom

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 Cd C:\Program Files (x86)\Microsoft Visual Studio\VB98
-vb6 /make C:\Code\wap\Demofrm.vbp /outdir C:\Code\wap\out
+vb6 /make C:\Code\wap\Demofrm.vbp /outdir C:\TeamCity\buildAgent\work\d7dda94f54f8bd8
