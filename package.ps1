771785a94fefaa3afaf368e39fe0bb85ec531694
hpsjadaun <hpjadaun@yahoo.com>
2017-07-14 10:54:39 +0530

    Remove files

--- package.ps1
+++
@@ -1,5 +0,0 @@
- $source = "C:\Code\wap"
-$destination = "C:\Code\out2.zip"
-
-Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::CreateFromDirectory($source, $destination)	
