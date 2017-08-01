94270358789af06545b18b9fb633473cca47a82b
hpsjadaun <hpjadaun@yahoo.com>
2017-08-01 13:05:46 +0530

    chnages

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\VB98\VB6.EXE" /make Demofrm.vbp
+"C:\Program Files (x86)\Microsoft Visual Studio\VB98\Wizards\PDWizard\PDCmdLn.exe" C:\Reg\wap\Demofrm.vbp /p "PackageDemo" /d DeployDemo /l C:\Code\Log1.log
